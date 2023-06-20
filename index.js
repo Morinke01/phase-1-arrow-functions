a7e520f2ba877fa254fa7855554fab1439a588c6
Morinke001 <morel1276.mj@gmail.com>
2023-06-20 11:39:49 +0300

    Assignment done

---
+++ index.js
@@ -0,0 +1,14 @@
+//arrow functions
+//Dividing two numbers
+const divide = (num1, num2) => {
+    num1=2000;
+    num2=100;
+    return num1/num2;
+}
+divide;
+//multiplying a number by itself
+const square = x => (x ** 2);
+square;
+// array function to add two numbers
+const add = (parameter1, parameter2) => parameter1 + parameter2;
+add;
